ef5ee90697dd569051f169662ba708741b2aa766
Kiss Bence <kiss.bence@burg.lo.hu>
2025-11-14 13:19:09 +0100

    links

--- kave/src/App.tsx
+++ kave/src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <nav>
+          <h1>Kávés oldal</h1>
+          <Link to="/">Home</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/products" element={<Products/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
